c86b4a9b988a8d0b2cf010ce91a4203a0b60f9c4
Tom Benner <tombenner@gmail.com>
2017-07-08 10:33:05 -0700

    Support usage of either a String or Hash for filter conditions

--- lib/reports_kit/reports/filter_types/boolean.rb
+++ lib/reports_kit/reports/filter_types/boolean.rb
@@ -7,21 +7,36 @@ module ReportsKit
         }
 
         def apply_conditions(records)
+          case conditions
+          when ::String
+            records.where("(#{conditions}) #{sql_operator} true")
+          when ::Hash
+            boolean_operator ? records.where(conditions) : records.not.where(conditions)
+          else
+            raise ArgumentError.new("Unsupported conditions type: '#{conditions}'")
+          end
+        end
+
+        def boolean_operator
           case criteria[:operator]
           when true, 'true'
-            records.where("(#{column}) = true")
+            true
           when false, 'false'
-            records.where("(#{column}) != true")
+            false
           else
             raise ArgumentError.new("Unsupported operator: '#{criteria[:operator]}'")
           end
+        end
+
+        def sql_operator
+          boolean_operator ? '=' : '!='
         end
 
         def valid?
           criteria[:operator].present?
         end
 
-        def column
+        def conditions
           settings[:conditions] || properties[:key]
         end
       end
