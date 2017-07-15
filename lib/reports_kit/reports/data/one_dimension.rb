093128d9ecaf547208544ecd1de7aca884c3f877
Tom Benner <tombenner@gmail.com>
2017-07-15 07:46:14 -0700

    Remove unused code

--- lib/reports_kit/reports/data/one_dimension.rb
+++ lib/reports_kit/reports/data/one_dimension.rb
@@ -37,27 +37,6 @@ module ReportsKit
           dimension_keys_values.delete('')
           dimension_keys_values
         end
-
-        def values
-          dimension_keys_values.values.map { |value| value.round(Generate::ROUND_PRECISION) }
-        end
-
-        def labels
-          dimension_keys.map do |key|
-            Utils.dimension_key_to_label(key, primary_dimension_with_measure, dimension_ids_dimension_instances)
-          end
-        end
-
-        def dimension_ids_dimension_instances
-          @dimension_ids_dimension_instances ||= begin
-            dimension_ids = dimension_keys_values.keys
-            Utils.dimension_to_dimension_ids_dimension_instances(primary_dimension_with_measure, dimension_ids)
-          end
-        end
-
-        def primary_dimension_with_measure
-          @primary_dimension_with_measure ||= DimensionWithMeasure.new(dimension: measure.dimensions.first, measure: measure)
-        end
       end
     end
   end
