ebf00f42dcb53bd037ea09d81c8668680ba52221
Tom Benner <tombenner@gmail.com>
2017-07-22 15:20:22 -0700

    Simplify the tests' add_label_suffix custom method

--- spec/support/config.rb
+++ spec/support/config.rb
@@ -2,9 +2,8 @@ ReportsKit.configure do |config|
   config.custom_methods = {
     format_percentage: -> (value) { "#{value.round(0)}%" },
     add_label_suffix: -> (data) {
-      data[:entities] = data[:entities].map do |entity|
+      data[:entities].each do |entity|
         entity.label = "#{entity.label} Foo" if entity.label
-        entity
       end
       data
     }
